38da4f09a7258dfab02eb7e46bb92515d6da6095
prameshnilakshan8 <109522478+prameshnilakshan8@users.noreply.github.com>
2022-07-18 21:56:59 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //════════════(00)════════════════\\
 //
-//          RED DRAGON v6.0
+//          pramesh lion bot v6.0
 //
 //════════════(00)════════════════\\
 
@@ -19,42 +19,42 @@ global.APIKeys = {
 
 //customize settings\\
 
-global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+global.alivemsg = `My name is pr⃢⃡amesh lion bot🔥
+My Creator is pramesh nilakshan 😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+94704472592'] //Owner number in aive msg
+global.premium = ['+94704472592'] //Owner Number info
+global.ownernomer = '+94704472592' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON࿐' //Bot Name
+global.ownername = '𝙥𝙧⃢⃡𝙖𝙢𝙚𝙨𝙝 𝙣𝙞⃢⃡𝙡𝙖𝙠𝙨𝙝𝙖𝙣' //Owner Name
+global.botname = ' ☬𝙥𝙧𝙖𝙢𝙚𝙨𝙝༆𝙡𝙞𝙤𝙣༒𝙡𝙞𝙤𝙣࿐' //Bot Name
 
-global.button = '☰ GITHUB ☰' //Costomize A Button Name In Alive Message
-global.btnurl = 'https://github.com/prabhasha2006' //Change Your Button's URL
+global.button = '𝙢𝙮 𝙬𝙝𝙖𝙩𝙨𝙖𝙥𝙥 𝙜𝙧𝙤𝙪𝙥' //Costomize A Button Name In Alive Message
+global.btnurl = 'https://chat.whatsapp.com/DKzR6vonZFzF8nlE8CEJPl' //Change Your Button's URL
 
 global.region = 'India, South India, Kerala' //Country Region
 
-global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
-global.prefa = ['','!','.','😂','❤️','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
+global.packname = '☬𝙥𝙧𝙖𝙢𝙚𝙨𝙝༆𝙡𝙞𝙤𝙣༒𝙗𝙤𝙩࿐' //Bot Pacage Name
+global.author = '😎 𝙥𝙧𝙖𝙢𝙚𝙨𝙝 ❯❯' //Author Name
+global.prefa = ['','!','.','🏵️','❤️','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor
-global.dragonemoji = '🧩' // Menu emoji
-global.footer = '©_Kumuthu.' //In list menu
+global.dragonemoji = '🏵️' // Menu emoji
+global.footer = 'ⓟ_𝙥𝙧𝙖𝙢𝙚𝙨𝙝.' //In list menu
 global.chatbotpre = 'true'  // Chat bot true/false
 
 global.ig = 'https://github.com/prabhasha2006' //Github Account!
 
 global.mess = { // COSTOMIZE WITH YOUR LANGUAGE
 	
-    success: 'Done ✅',
+    success: 'හරි ✅',
     admin: 'Only Admins can use it!😉',
     botAdmin: 'Bot wants to be an Admin😥',
     owner: 'Only Owner can use this!😉',
     group: 'Feature Used Only For Groups!',
     private: 'This Features Only For Private Chat!',
     bot: 'This Feature Is Only For Bot😝',
-    wait: '👉👈Pleas Wait...',
+    wait: '👉👈පොඩ්ඩක් ඉන්න...',
     error: 'Error!!! Maybe Api Key Is Expired🤔!',
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
 }
